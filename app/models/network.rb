03beded3f0a94e9a66980f4369deaace7d0a46d3
kalbozo <kalbozo1@gmail.com>
2019-01-14 11:26:40 -0700

    Heroku setup (#6)
    * modifying model for heroku
    
    * modifying model for heroku
    
    * modifying model for heroku
    
    * modifying model for heroku
    
    * modifying model for heroku
    
    * modifying model for heroku
    
    * modifying model for heroku
    
    * modifying model for heroku
    
    * setting up db for heroku

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,4 +1,10 @@
 class Network < ApplicationRecord
-    attr_accessor :href, :name, :city
-    has_and_belongs_to_many :searches
+    validates :href, presence: true
+    validates :city, presence: true
+    has_and_belongs_to_many :searches, required: false
+
+    before_save {
+        #puts JSON.parse(self)
+        self.city.downcase!
+    }
 end
