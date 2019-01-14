a6f59631740a4c2db6652f234c1b7ed567bdaec2
Kaleb.Bozorgzadeh <bozokal@linux-pc-8588.echostar.com>
2019-01-14 09:25:28 -0700

    modifying model for heroku

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -7,12 +7,13 @@ class NetworksController < ApplicationController
     if response.success?
       networks = JSON.parse(response.body)
       networks['networks'].each do |network|
-        begin
-          @networkModel = Network.where(:href => network['href']).first_or_create
-          Network.update(networkModel.id, :city => network['location']['city'], :longitude => network['location']['longitude'], :latitude => network['location']['latitude'],:name=> network['id'])
-        rescue
-          next
-        end
+        #begin
+          network_model = Network.where(:href => network['href']).first_or_create!
+          Network.update(network_model.id, :city => network['location']['city'], :longitude => network['location']['longitude'], :latitude => network['location']['latitude'],:name=> network['id'])
+          
+        #rescue
+        #  next
+        #end
       end
       #destroy all non-updated networks. This means they didnt exist on the api
       Network.where("updated_at < ?", current_time).destroy_all
