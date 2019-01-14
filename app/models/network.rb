6c7ab29ea9b307070639c75515fadbd76348d259
Kaleb.Bozorgzadeh <bozokal@linux-pc-8588.echostar.com>
2019-01-14 10:32:32 -0700

    modifying model for heroku

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,7 +1,7 @@
 class Network < ApplicationRecord
     #attr_accessor :href, :name, :city
     validates :href, presence: true
-    validates :city, presense: true
+    validates :city, presence: true
     has_and_belongs_to_many :searches, required: false
 
     before_save {
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,6 @@
 class Search < ApplicationRecord
     has_and_belongs_to_many :networks
-    validates :city, presense: true
+    validates :city, presence: true
 
     before_save {
         city.downcase!
