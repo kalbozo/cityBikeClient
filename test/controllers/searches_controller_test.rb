307ef5d00f42300f109d3b1a0242a500067364e7
kalbozo <kalbozo1@gmail.com>
2019-01-15 12:54:56 -0700

    Tests (#9)
    * adding rake task for scheduler
    
    * added sorting methods
    
    * adding controller tests and some error handling

--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -4,4 +4,12 @@ class SearchesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "no city param in search causes error json" do
+    get '/search'
+    assert JSON.parse(response.body)['error'].present?
+  end
+  test "no city param in stations causes error json" do
+    get '/stations'
+    assert JSON.parse(response.body)['error'].present?
+  end
 end
